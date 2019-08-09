1638aeebf268e680fc40bc47ebbd6d48bb63fbf8
kwanj-k <mwangikelvin23@gmail.com>
2019-08-09 17:27:29 +0300

    Deploy to heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn biserver.wsgi --log-file -
+release: python manage.py migrate
--- biserver/settings/base.py
+++ biserver/settings/base.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
