b2cc59392830fa24fa6d055b8911259341402ff9
kwanj-k <mwangikelvin23@gmail.com>
2019-08-10 19:25:02 +0300

    Add swagger docs

--- biserver/urls.py
+++ biserver/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+
+# set the title for the API.
+schema_view = get_swagger_view(title="BI Server")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('src.api.urls')),
+
+    path('', schema_view),
 ]
